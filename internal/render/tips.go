34c2e1248ff78ec679d4bca49f176501efa7bd90
Bahadır Kandemir <kandemir@gmail.com>
2023-01-29 15:57:39 +0200

    Add more tips

--- internal/render/tips.go
+++ internal/render/tips.go
@@ -1,12 +1,17 @@
 package render
 
-import "math/rand"
+import (
+	"time"
+)
 
 var tips = []string{
 	"Star [this repository](https://github.com/bahadir/bahadir) to give XP to the hero. Hero needs XP to level up.",
 	"Feel free to open a PR to add new elements to the map. Take a look at [Map Design](data/README.md) document.",
+	"Map is larger than the viewport. Hero can move around the map.",
 }
 
 func Tip() string {
-	return tips[rand.Intn(len(tips))]
+	days := int(time.Now().Unix() / (24 * 60 * 60))
+
+	return tips[days%len(tips)]
 }
